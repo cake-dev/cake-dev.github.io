7708206d6582316d1015a9df90c4b033f47bf853
cake-dev <developer.montana@gmail.com>
2022-09-30 14:51:20 -0600

    new file:   Project2/Data/burned_area_1985.csv 	new file:   Project2/Data/burned_area_2015.csv 	modified:   Project2/Data/western_ag.csv 	modified:   Project2/JS/runbutton.js 	modified:   Project2/data_notebook.ipynb 	modified:   Project2/geo_notebook.ipynb 	modified:   Project2/map.html

--- Project2/JS/runbutton.js
+++ Project2/JS/runbutton.js
@@ -1,7 +1,7 @@
-button = d3.select("body").append("button");
-button.text("Change Map");
-button.on("click", changeMap);
+button = d3.select("#legend_container").append("button");
+button.text("Change Year");
+button.on("click", changeYear);
 
-button = d3.select("body").append("button");
+button = d3.select("#legend_container").append("button");
 button.text("Change Size");
 button.on("click", changeSize);
