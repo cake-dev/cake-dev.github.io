40aab4db13faaa0e8a510ea47bba8c22b6d3d86c
cake-dev <developer.montana@gmail.com>
2022-10-16 21:32:06 -0600

    new file:   Project3/data/alaska_ice_2018_2022.TIFF 	modified:   Project3/js/leafscript.js 	modified:   Project3/leaflet_map.html

--- Project3/js/leafscript.js
+++ Project3/js/leafscript.js
@@ -1,26 +1,126 @@
-var map = L.map('map').setView([37.8, -96], 4);
 
-var osm = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-}).addTo(map);
+function fetchRaster(url) {
+    return fetch(url)
+        .then(response => response.arrayBuffer())
+        .then(arrayBuffer => {
+            parseGeoraster(arrayBuffer).then(georaster => { return georaster });
+        });
+}
 
-var windSpeed = L.leafletGeotiff(
-    url = 'data/sea_ice_10_14_22.TIFF',
-    options = {
-        band: 0,
-        displayMin: -1,
-        displayMax: 1,
-        name: 'NDVI',
-        colorScale: 'rainbow',
-        clampLow: false,
-        clampHigh: true,
-        //vector:true,
-        arrowSize: 20,
-    }
-).addTo(map);
+function fetchJSON(url) {
+    return fetch(url)
+        .then(function (response) {
+            return response.json();
+        });
+}
+
+async function createMap() {
+    var map = L.map('map', { zoomControl: false });
+
+    var Esri_WorldImagery = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Terrain_Base/MapServer/tile/{z}/{y}/{x}', {
+        attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
+    }).addTo(map);
+
+    var url_to_geotiff_file = "data/alaska_ice_2018_2022.TIFF";
+
+    fetch(url_to_geotiff_file)
+        .then(response => response.arrayBuffer())
+        .then(arrayBuffer => {
+            parseGeoraster(arrayBuffer).then(georaster => {
+                const min = georaster.mins[0];
+                const max = georaster.maxs[0];
+                const range = georaster.ranges[0];
+
+                console.log(georaster)
+
+                // available color scales can be found by running console.log(chroma.brewer);
+                // console.log(chroma.brewer);
+                var scale = chroma.scale("BuPu");
+
+                var layer = new GeoRasterLayer({
+                    georaster: georaster,
+                    opacity: 0.5,
+                    pixelValuesToColorFn: function (pixelValues) {
+                        var pixelValue = pixelValues[0]; // there's just one band in this raster
+
+                        // if there's zero value, don't return a color
+                        if (pixelValue === 0) return null;
+
+                        // scale to 0 - 1 used by chroma
+                        var scaledPixelValue = (pixelValue - min) / range;
+
+                        var color = scale(scaledPixelValue).hex();
+
+                        return color;
+                    },
+                    resolution: 256
+                });
+                var layer2 = new GeoRasterLayer({
+                    georaster: georaster,
+                    opacity: 0.5,
+                    pixelValuesToColorFn: function (pixelValues) {
+                        var pixelValue = pixelValues[1]; // there's just one band in this raster
+
+                        // if there's zero value, don't return a color
+                        if (pixelValue === 0) return null;
+
+                        // scale to 0 - 1 used by chroma
+                        var scaledPixelValue = (pixelValue - min) / range;
+
+                        var color = scale(scaledPixelValue).hex();
+
+                        return color;
+                    },
+                    resolution: 256
+                });
+
+                var ice_layers = {
+                    "2018": layer,
+                    "2022": layer2
+                }
+
+                var layerControl = L.control.layers(ice_layers).addTo(map);
+
+                // console.log("layer:", layer);
+                layer.addTo(map);
 
 
+                map.fitBounds(layer.getBounds());
+                map.setView([65.440002, -152.358398], 4);
+            });
+        });
+
+    console.log(map._layers)
+
+    // var data = fetchJSON('data/mtbs_perims_us_2020_large.geojson')
+    //     .then(function (data) { return data })
+    //     .then(function (data) {
+    //         var geojsonLayer = L.geoJson(data, {
+    //             style: function (feature) {
+    //                 return {
+    //                     color: 'red',
+    //                     weight: 1,
+    //                     opacity: 0.5,
+    //                     fillOpacity: 0.1
+    //                 };
+    //             }
+    //         }).addTo(map);
+    //     });
+}
+createMap();
+
+
+
+
+// map.dragging.disable();
+// map.touchZoom.disable();
+// map.doubleClickZoom.disable();
+// map.scrollWheelZoom.disable();
+// map.boxZoom.disable();
+// map.keyboard.disable();
+// if (map.tap) map.tap.disable();
+// document.getElementById('map').style.cursor = 'default';
+document.getElementsByClassName('leaflet-control-attribution')[0].style.display = 'none';
 // var imageUrl = 'https://maps.lib.utexas.edu/maps/historical/newark_nj_1922.jpg';
 // var errorOverlayUrl = 'https://cdn-icons-png.flaticon.com/512/110/110686.png';
 // var altText = 'Image of Newark, N.J. in 1922. Source: The University of Texas at Austin, UT Libraries Map Collection.';
